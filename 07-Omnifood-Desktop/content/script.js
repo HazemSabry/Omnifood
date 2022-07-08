4da9d75ee30bf1c195e43d349de2ee30a6f18bb5
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2022-07-08 03:57:18 +0200

    Add files via upload

--- 07-Omnifood-Desktop/content/script.js
+++ 07-Omnifood-Desktop/content/script.js
@@ -11,10 +11,48 @@ console.log(h1)
 //   h1.style.padding = "5rem";
   
 // });
+///////////////////////////////
+// set current year
 
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear
+
+
+////////////////////////////////////////
+// make mobile navigation work 
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNavEl.addEventListener("click",function (){
+  headerEl.classList.toggle("nav-open");
+});
+
+//////////////////////////////////////////
+// smooth scrolling animation
+
+const allLink =document.querySelectorAll("a:link");
+allLink.forEach(function (link){
+  link.addEventListener("click",function(e){
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // console.log(href);
+
+    ///////// scroll back to top //////
+    if(href == "#") window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    ///////// scroll to other link //////
+    if (href != "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({behavior: "smooth"})
+    };
+    ///////// close mobile navigation ///////
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    };
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
